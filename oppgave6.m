716071737e40d0742eec772c8f293a7ca155c10c
chrisbso <chrisbso@stud.ntnu.no>
2016-04-08 14:24:28 +0200

    Christoffers oppgave 6

--- oppgave6.m
+++ oppgave6.m
@@ -5,6 +5,31 @@ function matrix = oppgave6 (N)
  dTheta = pi/nTheta; %%=0.5*
  theta = [0:dTheta:pi];
  
+ im = zeros(N,N);
+ x = zeros(1,N);
+ s = zeros(N,N);
+ sm = zeros(1,2*N-1);
+ y = x;
+ a = 1;
+ for u = 1: N
+     x(u) = ((2*u-N-1)/2)*a;
+     y(u) = ((2*u-N-1)/2)*a;
+ end
+ 
+ for m = 1:(2*N-1)
+     sm(m) = -(N-m)*a/sqrt(2)
+ end
+ 
+ for thetaIter = theta(1):dTheta:theta(end)
+    for i = 1:N
+        for j = 1:N
+        im(i,j)=x(i)*cos(thetaIter) + y(j)*sin(thetaIter);
+        end
+    end
+ end
+ 
+ 
+         
  
  figure;
  imagesc(matrix);
