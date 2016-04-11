f46fe3a99df4960c89236cabffd5e1d9b8ba3dc5
Halvorsg <halvor.snersrud@gmail.com>
2016-04-11 18:04:15 +0200

    Forsøkt på oppgave ni
    Nå skal jeg lage middag

---
+++ oppgave9MakeFilter.asv
@@ -0,0 +1,55 @@
+%her lager vi filteret
+function filter = oppgave9MakeFilter(fantom)
+
+close all
+[M,N] = size(fantom);
+skarp = phantom(M);
+%Normaliserer matrisen for å få frem et ok bilde 
+mellom = ones(N,M).*min(min(fantom));
+ny = fantom-mellom;
+fantom = ny./max(max(ny));
+
+mellom = ones(N,M).*min(min(skarp));
+ny = skarp-mellom;
+skarp = ny./max(max(ny));
+figure, imshow(abs(skarp)), colormap gray
+title('Skarp')
+figure, imshow(abs(fantom)), colormap gray
+title('Projeksjon')
+
+F = fftshift(fft2(double(skarp)));
+G = fftshift(fft2(double(fantom)));
+
+Flog = log(abs(F));
+Glog = log(abs(G));
+figure;
+subplot(1,2,1), imagesc(Flog,[-2,10]), axis square, colorbar, title('skarp');
+subplot(1,2,2), imagesc(Glog,[-2,10]), axis square, colorbar, title('fantom');
+
+% %for å teste om den fungere
+% diff = Flog-Glog;
+% figure, imshow(diff,[0 5]), colormap gray;
+
+%finner A som et filter
+filter = zeros(N,N);
+for i = 1:N
+    for j = 1:N
+        filter(i,j)= F(i,j)/G(i,j);
+    end
+end
+
+H = zeros(N,N);
+for i = 1:N
+    for j = 1:N
+        H(i,j)= filter(i,j)*G(i,j);
+    end
+end
+
+imageC = ifft2(ifftshift(H));
+figure, imshow(abs(imageC)), colormap gray
+title('Image C  uten normalisering')
+
+
+
+
+
--- oppgave9filter.m
+++ oppgave9filter.m
@@ -1,17 +1,19 @@
 %Oppgave 9 filter
 function oppgave9filter(mikkelsen)
 close all
+fantom = load('phantom256backprojected.mat');
+filter = oppgave9MakeFilter(fantom.phantom256);
+close all
 %Normaliserer matrisen for å få frem et ok bilde 
 [N,M]=size(mikkelsen);
 mellom = ones(N,M).*min(min(mikkelsen));
 ny = mikkelsen-mellom;
-sexyMikkelsen = ny./max(max(ny));
-imageA = sexyMikkelsen;
-figure, imshow(imageA)
-title('Image A - Original')
+imageA = ny./max(max(ny));
+
+figure, imshow(imageA), title('Original')
 
 %Fouriertransformerer bildet
-fftA = fft2(double(imageA));
+fftA = fft2(imageA);
 figure, imshow(abs(fftshift(fftA)),[0 10]), colormap gray
 title('FFTA shifted')
 %kaller den shiftede transformasjonen for fftB
@@ -19,36 +21,35 @@ fftB = fftshift(fftA);
 
 %Setter verdier i sentrum lik null
 [N,M]=size(fftA);
-senter = floor((M)/2)+1;
+%senter = floor((M)/2)+1;
 
-for R = 0:1:180
+% for R = 0:1:180
+%     for i = 1:length(mikkelsen)
+%         for j = 1:length(mikkelsen)
+%             if R^2 < (i-senter)^2+(j-senter)^2 && (i-senter)^2+(j-senter)^2 <= (R+1)^2
+%                 fftB(i,j) = fftB(i,j)*R; %%%DETTE ER BARE ANTAGELSER TIL FILTER
+%             end
+%         end
+%     end
+% end
+
+
+H = zeros(N,N);
     for i = 1:length(mikkelsen)
         for j = 1:length(mikkelsen)
-            if R^2 < (i-senter)^2+(j-senter)^2 && (i-senter)^2+(j-senter)^2 <= (R+1)^2
-                fftB(i,j) = fftB(i,j)*(R+0.1); %%%DETTE ER BARE ANTAGELSER TIL FILTER
-            end
+           H(i,j) = filter(i,j)*fftB(i,j);
         end
     end
-end
 
-%Viser transformasjonen med verdier i sentrum lik null
-figure, imshow(abs(fftB),[0 10]), colormap gray
-title('FFTB')      
+%Transformerer tilbake
+filtrert = ifft2(ifftshift(H));
 
-%Image C er fftB transformert tilbake og skal derfor etter normalisering
-%Ligne på orginalbildet
-imageC = ifft2(ifftshift(fftB));
-figure, imshow(abs(imageC)), colormap gray
-title('Image C  uten normalisering')
+[N,M]=size(filtrert);
+mellom = ones(N,M).*min(min(filtrert));
+ny = filtrert-mellom;
+filtrert = ny./max(max(ny));
 
-%Normaliserer imageC
-[N,M]=size(imageC);
-mellom1 = ones(N,M).*min(min(imageC));
-ny1 = imageC-mellom1;
-sexyImageC = ny1./max(max(ny1));
-
-figure, imshow(real(sexyImageC)), colormap gray
-title('Image C  med normalisering')
+figure, imshow(filtrert), title('Filtrert')
 end
 
             
