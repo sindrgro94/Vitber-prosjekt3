0470d0b87ecc11395944928019205dbf6307e728
Halvorsg <halvor.snersrud@gmail.com>
2016-04-12 13:48:25 +0200

    Oppgave 9

--- oppgave9filter.m
+++ oppgave9filter.m
@@ -1,11 +1,11 @@
 %Oppgave 9 filter
-function oppgave9filter(mikkelsen)
-%close all
+function oppgave9filter(tilbakeProjeksjon)
+close all
 
 %Normaliserer matrisen for å få frem et ok bilde 
-[N,M]=size(mikkelsen);
-mellom = ones(N,M).*min(min(mikkelsen));
-ny = mikkelsen-mellom;
+[N,M]=size(tilbakeProjeksjon);
+mellom = ones(N,M).*min(min(tilbakeProjeksjon));
+ny = tilbakeProjeksjon-mellom;
 imageA = ny./max(max(ny));
 
 
@@ -21,8 +21,8 @@ senter = floor((M)/2)+1;
 
 k = 1;
 for R = 0:1:181
-    for i = 1:length(mikkelsen)
-        for j = 1:length(mikkelsen)
+    for i = 1:length(tilbakeProjeksjon)
+        for j = 1:length(tilbakeProjeksjon)
             if R<15 && R^2 < (i-senter)^2+(j-senter)^2 && (i-senter)^2+(j-senter)^2 <= (R+1)^2
                  fftB(i,j) = fftB(i,j)*k*R; %%%DETTE ER BARE ANTAGELSER TIL FILTER
             elseif R>=15 && R^2 < (i-senter)^2+(j-senter)^2 && (i-senter)^2+(j-senter)^2 <= (R+1)^2
@@ -38,7 +38,7 @@ filtrertNy = ifft2(ifftshift(fftB));
 
 %Illustrerer resultatet
 figure;
-subplot(1,2,2),imshow(filtrertNy), title('Filtrert linneært')
+subplot(1,2,2),imshow((filtrertNy)), title('Filtrert linneært')
 subplot(1,2,1),imshow(imageA), title('Original')
 figure;
 subplot(1,2,1),imshow(real(fftshift(fftA))), title('Original Fourier')
