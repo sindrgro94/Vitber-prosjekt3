4c88420b17badd6d95c41fa9c682c66a734a3190
Halvorsg <halvor.snersrud@gmail.com>
2016-04-15 14:31:45 +0200

    graveyard

--- test.m
+++ test.m
@@ -1,12 +1,13 @@
 %test
 N = 256;
-% im1 = phantom(N);
-% F = fftshift(fft2(im1));
-% im2 = ifft2(ifftshift(F));
-% figure;
-% subplot(1,2,1), imagesc(im1), colormap('gray'), axis square;
-% subplot(1,2,2), imagesc(im2), colormap('gray'), axis square;
-% title('Like bilder')
+im1 = phantom(N);
+F = fftshift(fft2(im1));
+im2 = ifft2(ifftshift(F));
+figure;
+title('like bilder')
+subplot(1,2,1), imagesc(im1), colormap('gray'), axis square;
+subplot(1,2,2), imagesc(im2), colormap('gray'), axis square;
+
 
 figure;
 I=phantom(N);
